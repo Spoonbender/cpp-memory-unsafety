2a913e976b6255f52f2768f53dba849aaf0c6669
Maayan Hanin <maayan.asa.hanin@gmail.com>
2019-12-14 12:39:51 +0200

    accessing deallocated memory - error

--- CppDemo/CppDemo.cpp
+++ CppDemo/CppDemo.cpp
@@ -1,7 +1,22 @@
 #include "pch.h"
 #include <iostream>
 
+class Student {
+public:
+	int id;
+	int age;
+};
+
+Student* GetStudent(int id, int age) {
+	Student *temp = new Student();
+	temp->id = id;
+	temp->age = age;
+	return temp;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Student *student = GetStudent(1, 34);
+	delete student;
+	printf("Id = %d Age = %d\n", student->id, student->age);
 }
