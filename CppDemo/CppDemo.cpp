0ce09aa784811e77ef6a849dc3317bc9bc4c983d
Maayan Hanin <maayan.asa.hanin@gmail.com>
2019-12-14 12:28:35 +0200

    Invalid pointer on stack demo

--- CppDemo/CppDemo.cpp
+++ CppDemo/CppDemo.cpp
@@ -1,7 +1,23 @@
 #include "pch.h"
 #include <iostream>
 
+struct Student {
+public:
+	int id;
+	int age;
+};
+
+Student *GetStudent(int id, int age) {
+	Student temp;
+	temp.id = id;
+	temp.age = age;
+	return &temp;
+}
+
 int main()
 {
+	Student* first = GetStudent(1, 34);
+	Student* second = GetStudent(2, 27);
+	printf("First Id = %d Age = %d\n", first->id, first->age);
     std::cout << "Hello World!\n"; 
 }
